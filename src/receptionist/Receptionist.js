703dd404613fe670e0355b16c8370845efa9353a
DattatrayMangrulkar <dattatray.m.2496@gmail.com>
2024-02-04 23:08:18 +0530

    commiting frontend code in one go....

---
+++ src/receptionist/Receptionist.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+function Receptionist(){
+    return <div className="container fluid">
+        <div className="row">
+            <div className="col-sm-6">
+                <div className="navbar navbar-expand-sm navbar-light bg-light">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to={"/receptionist/appointment"}>Show Appointments</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to={"/"}>Add Service</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <br></br>
+        <Outlet></Outlet>
+    </div>
+}
+export default Receptionist;
